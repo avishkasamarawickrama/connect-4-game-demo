16c62601cd499c1e5d60cc9e50b20bf792b4d3fd
avishkasamarawickrama <samarawickramaavishka@gmail.com>
2024-03-17 22:59:02 +0530

    add code

---
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -0,0 +1,35 @@
+package lk.ijse.dep.service;
+
+import java.util.Random;
+
+public class AiPlayer extends Player {
+    public AiPlayer(Board board) {
+
+        super(board);
+    }
+
+    @Override
+    public void movePiece(int col) {
+
+        do {
+            col = (int) (Math.random() * 6);
+
+
+        } while (!board.isLegalMove(col));
+
+        if (board.isLegalMove(col)) {
+            board.updateMove(col, Piece.GREEN);
+            board.getBoardUI().update(col, false);
+
+            if (board.findWinner().getWinningPiece().equals(Piece.EMPTY)) {
+                if (!board.existLegalMoves()) {
+                    board.getBoardUI().notifyWinner(new Winner(Piece.EMPTY));
+                }
+            } else {
+                board.getBoardUI().notifyWinner(board.findWinner());
+            }
+        }
+
+
+    }
+}
